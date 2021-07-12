cbaeb980fe9aeae86c0c7c789913bd9e22263049
Damir Bubanović <damir.bubanovic@yahoo.com>
2021-07-12 18:09:26 +0200

    OK final

--- guess.py
+++ guess.py
@@ -20,25 +20,3 @@ for attempt in range(attempts, 0, -1):
         console.print(f'That \'s right the number is {guess}', style="bold green")
         break
 
-
-
-
-
-
-
-
-
-
-
-
-# number = random.randrange(1,10)
-# guess = 0
-
-# while guess != number:
-#   guess = int(input(f'Guess what number am I thinking between 1 and 10?'))
-#   if guess < number:
-#       print(f'Wrong Too Low')
-#   elif guess > number:
-#       print(f'Wrong Too High')
-
-# print(f'That \'s right the number is {guess}')
